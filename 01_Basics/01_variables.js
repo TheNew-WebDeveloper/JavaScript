8a195dfb5ec8d6dd0a501c4e9009380a74c79b71
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2024-11-18 19:56:20 +0000

    changes commit done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 236;
+let accountName = "Rio";
+var accouuntPassword = "Rio@123";
+accountCity = "Hyderabad";
+
+console.table([accountId,accountName,accouuntPassword,accountCity]);
+
+let accountAddress;
+
+console.log(accountAddress);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+alert("hello"); //we are using nodejs, not browser
+
+console.log(3 + 3); console.log("Rio"); //code Readability should be high
+
+/*
+--- Data Types ---
+--01.Primitive Data
+number => 2^53
+string => " "
+boolean => true/fasle
+null => standalone value
+undefined => not yet defined
+bigInt => 
+symbol => unique
+--02.Non-Primitive
+Array
+
+*/
