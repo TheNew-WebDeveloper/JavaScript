73adeac04d925e096a2e764f0e5568d927d0567d
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2024-11-19 05:07:41 +0000

    commit done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,62 @@
+// let score = "33";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valInNumber = Number(score);
+// console.log(typeof(valInNumber));
+
+// let val = "33abc"
+// val = Number(val)
+// console.log(val,typeof(val));
+
+// /*
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1, "false" => 0;
+// */
+
+// let x = 1;
+// x = Boolean(x);
+// console.log(x,typeof(x));
+
+// // 1 => true; 0 => false
+// // "" => false; "Rio" => true
+
+// let y = 33;
+// y = String(y);
+// console.log(y,typeof(y));
+
+
+//---------------Operations------------------
+let value = 3;
+value = -value;
+console.log(value);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "wel"
+// let str2 = "come"
+// console.log(str1+str2);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log(1+"2"+3);
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// console.log(+true); //1
+// console.log(true+); //invalid code
+// console.log(+""); //0
+
+let num1, num2 ,num3 ,num4;
+num1 =num2 =num3 =num4 = 3+3;
+
+let gameCounter = 100;
+console.log(gameCounter++);
+console.log(++gameCounter);
+
